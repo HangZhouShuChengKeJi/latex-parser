43c259973862e27e56f62e6215bd64e3fc36d9f3
小天 <yufei.han@91chengguo.com>
2020-12-14 18:25:52 +0800

    修复bug

--- src/main/java/com/orange/latex/CmdDefinitions.java
+++ src/main/java/com/orange/latex/CmdDefinitions.java
@@ -19,8 +19,8 @@ public class CmdDefinitions {
     static {
         atomMap.put(Cmds.CMD_FRAC, new CmdAtomDefinition(Cmds.CMD_FRAC, FractionAtom.class));
         atomMap.put(Cmds.CMD_SQRT, new CmdAtomDefinition(Cmds.CMD_SQRT, SqrtAtom.class));
-        atomMap.put(Cmds.CMD_PM, new CmdAtomDefinition(Cmds.CMD_PM));
         atomMap.put(Cmds.CMD_PI, new CmdAtomDefinition(Cmds.CMD_PI));
+        atomMap.put(Cmds.CMD_pm, new CmdAtomDefinition(Cmds.CMD_pm));
     }
 
     public static CmdAtomDefinition get(String cmd) {
