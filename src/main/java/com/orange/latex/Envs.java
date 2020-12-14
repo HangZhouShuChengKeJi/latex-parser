901057f99bbcfe36f7215d9cbbc0afeace27692a
小天 <yufei.han@91chengguo.com>
2020-12-14 15:38:27 +0800

    增加更多环境的枚举

--- src/main/java/com/orange/latex/Envs.java
+++ src/main/java/com/orange/latex/Envs.java
@@ -5,5 +5,36 @@ package com.orange.latex;
  * @date 2020/12/11 10:04
  */
 public class Envs {
-    public static final String ENV_ARRAY = "array";
+    /**
+     * 数组
+     */
+    public static final String ENV_array   = "array";
+    /**
+     * 案例组（一般用于方程组、不等式组等）
+     */
+    public static final String ENV_cases   = "cases";
+    /**
+     * 无括号的矩阵
+     */
+    public static final String ENV_matrix  = "matrix";
+    /**
+     * 竖线括号的矩阵
+     */
+    public static final String ENV_vmatrix = "vmatrix";
+    /**
+     * 双竖线括号的矩阵
+     */
+    public static final String ENV_Vmatrix = "Vmatrix";
+    /**
+     * 小括号矩阵
+     */
+    public static final String ENV_pmatrix = "pmatrix";
+    /**
+     * 中括号矩阵
+     */
+    public static final String ENV_bmatrix = "bmatrix";
+    /**
+     * 大括号矩阵
+     */
+    public static final String ENV_Bmatrix = "Bmatrix";
 }
