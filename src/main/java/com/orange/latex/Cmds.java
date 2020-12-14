cedfc980c946fc0d0d6310b692aff9442657d1c1
小天 <yufei.han@91chengguo.com>
2020-12-14 16:16:34 +0800

    增加一些命令枚举

--- src/main/java/com/orange/latex/Cmds.java
+++ src/main/java/com/orange/latex/Cmds.java
@@ -35,6 +35,306 @@ public class Cmds {
     public static final String CMD_SUB = "sub";
 
     /**
+     * 圆周率符号：π
+     */
+    public static final String CMD_PI = "pi";
+
+    ////////  关系运算符  ////////
+
+    /**
+     * 符号： ±
+     */
+    public static final String CMD_pm        = "pm";
+    /**
+     * 符号： ×
+     */
+    public static final String CMD_times     = "times";
+    /**
+     * 符号： ÷
+     */
+    public static final String CMD_div       = "div";
+    /**
+     * 符号： ∣
+     */
+    public static final String CMD_mid       = "mid";
+    /**
+     * 符号： ∤
+     */
+    public static final String CMD_nmid      = "nmid";
+    /**
+     * 符号： ⋅
+     */
+    public static final String CMD_cdot      = "cdot";
+    /**
+     * 符号： ∘
+     */
+    public static final String CMD_circ      = "circ";
+    /**
+     * 符号： ∗
+     */
+    public static final String CMD_ast       = "ast";
+    /**
+     * 符号： ⨀
+     */
+    public static final String CMD_bigodot   = "bigodot";
+    /**
+     * 符号： ⨂
+     */
+    public static final String CMD_bigotimes = "bigotimes";
+    /**
+     * 符号： ⨁
+     */
+    public static final String CMD_bigoplus  = "bigoplus";
+    /**
+     * 符号： ≤
+     */
+    public static final String CMD_leq       = "leq";
+    /**
+     * 符号： ≥
+     */
+    public static final String CMD_geq       = "geq";
+    /**
+     * 符号： ≠
+     */
+    public static final String CMD_neq       = "neq";
+    /**
+     * 符号： ≈
+     */
+    public static final String CMD_approx    = "approx";
+    /**
+     * 符号： ≡
+     */
+    public static final String CMD_equiv     = "equiv";
+    /**
+     * 符号： ∑
+     */
+    public static final String CMD_sum       = "sum";
+    /**
+     * 符号： ∏
+     */
+    public static final String CMD_prod      = "prod";
+    /**
+     * 符号： ∐
+     */
+    public static final String CMD_coprod    = "coprod";
+
+    ////////  集合运算符  ////////
+
+    /**
+     * 符号： ∅
+     */
+    public static final String CMD_emptyset = "emptyset";
+    /**
+     * 符号： ∈
+     */
+    public static final String CMD_in       = "in";
+    /**
+     * 符号： ∉
+     */
+    public static final String CMD_notin    = "notin";
+    /**
+     * 符号： ⊂
+     */
+    public static final String CMD_subset   = "subset";
+    /**
+     * 符号： ⊃
+     */
+    public static final String CMD_supset   = "supset";
+    /**
+     * 符号： ⊆
+     */
+    public static final String CMD_subseteq = "subseteq";
+    /**
+     * 符号： ⊇
+     */
+    public static final String CMD_supseteq = "supseteq";
+    /**
+     * 符号： ⋂
+     */
+    public static final String CMD_bigcap   = "bigcap";
+    /**
+     * 符号： ⋃
+     */
+    public static final String CMD_bigcup   = "bigcup";
+    /**
+     * 符号： ⋁
+     */
+    public static final String CMD_bigvee   = "bigvee";
+    /**
+     * 符号： ⋀
+     */
+    public static final String CMD_bigwedge = "bigwedge";
+    /**
+     * 符号： ⨄
+     */
+    public static final String CMD_biguplus = "biguplus";
+    /**
+     * 符号： ⨆
+     */
+    public static final String CMD_bigsqcup = "bigsqcup";
+
+    ////////  对数运算符  ////////
+
+    /**
+     * 符号： log
+     */
+    public static final String CMD_log = "log";
+    /**
+     * 符号： lg
+     */
+    public static final String CMD_lg  = "lg";
+    /**
+     * 符号： ln
+     */
+    public static final String CMD_ln  = "ln";
+
+    ////////  三角运算符  ////////
+
+    /**
+     * 符号： ⊥
+     */
+    public static final String CMD_bot   = "bot";
+    /**
+     * 符号： ∠
+     */
+    public static final String CMD_angle = "angle";
+    /**
+     * 符号： sin
+     */
+    public static final String CMD_sin   = "sin";
+    /**
+     * 符号： cos
+     */
+    public static final String CMD_cos   = "cos";
+    /**
+     * 符号： tan
+     */
+    public static final String CMD_tan   = "tan";
+    /**
+     * 符号： cot
+     */
+    public static final String CMD_cot   = "cot";
+    /**
+     * 符号： sec
+     */
+    public static final String CMD_sec   = "sec";
+    /**
+     * 符号： csc
+     */
+    public static final String CMD_csc   = "csc";
+
+    ////////  微积分运算符  ////////
+
+    /**
+     * 符号： ′
+     */
+    public static final String CMD_prime  = "prime";
+    /**
+     * 符号： ∫
+     */
+    public static final String CMD_int    = "int";
+    /**
+     * 符号： ∬
+     */
+    public static final String CMD_iint   = "iint";
+    /**
+     * 符号： ∭
+     */
+    public static final String CMD_iiint  = "iiint";
+    /**
+     * 符号： ⨌
+     */
+    public static final String CMD_iiiint = "iiiint";
+    /**
+     * 符号： ∮
+     */
+    public static final String CMD_oint   = "oint";
+    /**
+     * 符号： lim
+     */
+    public static final String CMD_lim    = "lim";
+    /**
+     * 符号： ∞
+     */
+    public static final String CMD_infty  = "infty";
+    /**
+     * 符号： ∇
+     */
+    public static final String CMD_nabla  = "nabla";
+
+    ////////  逻辑运算符  ////////
+
+    /**
+     * 符号： ∵
+     */
+    public static final String CMD_because   = "because";
+    /**
+     * 符号： ∴
+     */
+    public static final String CMD_therefore = "therefore";
+    /**
+     * 符号： ∀
+     */
+    public static final String CMD_forall    = "forall";
+    /**
+     * 符号： ∃
+     */
+    public static final String CMD_exists    = "exists";
+
+    ////////  箭头符号  ////////
+
+    /**
+     * 符号： ↑
+     */
+    public static final String CMD_uparrow        = "uparrow";
+    /**
+     * 符号： ↓
+     */
+    public static final String CMD_downarrow      = "downarrow";
+    /**
+     * 符号： ⇑
+     */
+    public static final String CMD_Uparrow        = "Uparrow";
+    /**
+     * 符号： ⇓
+     */
+    public static final String CMD_Downarrow      = "Downarrow";
+    /**
+     * 符号： →
+     */
+    public static final String CMD_rightarrow     = "rightarrow";
+    /**
+     * 符号： ←
+     */
+    public static final String CMD_leftarrow      = "leftarrow";
+    /**
+     * 符号： ⇒
+     */
+    public static final String CMD_Rightarrow     = "Rightarrow";
+    /**
+     * 符号： ⇐
+     */
+    public static final String CMD_Leftarrow      = "Leftarrow";
+    /**
+     * 符号： ⟶
+     */
+    public static final String CMD_longrightarrow = "longrightarrow";
+    /**
+     * 符号： ⟵
+     */
+    public static final String CMD_longleftarrow  = "longleftarrow";
+    /**
+     * 符号： ⟹
+     */
+    public static final String CMD_Longrightarrow = "Longrightarrow";
+    /**
+     * 符号： ⟸
+     */
+    public static final String CMD_Longleftarrow  = "Longleftarrow";
+
+    ////////  常用命令  ////////
+
+    /**
      * 分数
      */
     public static final String CMD_FRAC = "frac";
@@ -44,13 +344,13 @@ public class Cmds {
     public static final String CMD_SQRT = "sqrt";
 
     /**
-     * 加减符号：±
+     * 和分数相同的效果
      */
-    public static final String CMD_PM = "pm";
+    public static final String CMD_over = "over";
 
     /**
-     * 圆周率符号：π
+     * 符号： "..."
      */
-    public static final String CMD_PI = "pi";
+    public static final String CMD_cdots = "cdots";
 
 }
